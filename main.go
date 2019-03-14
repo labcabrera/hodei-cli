d77779fd21d1dbb081abe02e8857efa3aa1ab074
Luis Cabrera <luis.barragan.gfi@cnppartners.eu>
2019-03-14 16:31:34 +0100

    Back to snapshot

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/streadway/amqp"
 )
 
-const version = "0.1.0"
+const version = "0.2.0-SNAPSHOT"
 const exchangeSepa = "cnp.sepa"
 const exchangeReferential = "cnp.referential"
 
