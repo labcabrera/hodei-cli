7a476ed99dd4fa15cc6d7d071e7b9fc0272a3687
Luis Cabrera <luis.barragan.gfi@cnppartners.eu>
2019-03-18 15:49:15 +0100

    Format

--- main.go
+++ main.go
@@ -14,22 +14,22 @@ const exchangeReferential = "cnp.referential"
 func main() {
 
 	// Arguments
-	pullCountries  := flag.Bool("pull-countries",  false, "Pull countries over referential API")
-	pullProducts   := flag.Bool("pull-products",   false, "Pull products and agreements over referential API")
-	pullPolicies   := flag.Bool("pull-policies",   false, "Pull policies")
-	pullPerson     := flag.Bool("pull-person",     false, "Pull person from referential API")
-	pullLegal      := flag.Bool("pull-legal",      false, "Pull legal entity from referential API")
+	pullCountries	:= flag.Bool("pull-countries",	false,	"Pull countries over referential API")
+	pullProducts	:= flag.Bool("pull-products",	false,	"Pull products and agreements over referential API")
+	pullPolicies	:= flag.Bool("pull-policies",	false,	"Pull policies")
+	pullPerson		:= flag.Bool("pull-person",		false,	"Pull person from referential API")
+	pullLegal		:= flag.Bool("pull-legal",		false,	"Pull legal entity from referential API")
 
-	id             := flag.String("id",            "",    "Entity MongoDB identifier")
-	externalCode   := flag.String("external-code", "",    "Entity external code")
-	product        := flag.String("product",       "",    "Product name")
-	agreement      := flag.String("agreement",     "",    "Agreement external code")
-	argIban           := flag.String("iban",       "",    "IBAN validation")
+	id				:= flag.String("id",			"",		"Entity MongoDB identifier")
+	externalCode	:= flag.String("external-code",	"",		"Entity external code")
+	product			:= flag.String("product",		"",		"Product name")
+	agreement		:= flag.String("agreement",		"",		"Agreement external code")
+	argIban			:= flag.String("iban",			"",		"IBAN validation")
 
-	username       := flag.String("u",             "",    "Username")
-	authorities    := flag.String("a",             "",    "Authorities (coma separated list)")
-	verbose        := flag.Bool("v",               false, "Verbose")
-	printVersion   := flag.Bool("version",         false, "Print version")
+	username		:= flag.String("u",				"",		"Username")
+	authorities		:= flag.String("a",				"",		"Authorities (coma separated list)")
+	verbose			:= flag.Bool("v",				false,	"Verbose")
+	printVersion	:= flag.Bool("version",			false,	"Print version")
 	
 	flag.Parse()
 	
