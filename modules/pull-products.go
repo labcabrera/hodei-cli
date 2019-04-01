15dec52c55488f25fea4df0dab64bd91b95e6212
Luis Cabrera <luis.barragan.gfi@cnppartners.eu>
2019-04-01 17:37:50 +0200

    Product/agreement security headers

--- modules/pull-products.go
+++ modules/pull-products.go
@@ -5,25 +5,35 @@ import (
 	"log"
 
 	"github.com/labcabrera/hodei-cli/client"
+	"github.com/streadway/amqp"
 )
 
 const PullProductsCmd = "pull-products"
 
 type PullProductsOptions struct {
-	Verbose bool
-	Help    bool
+	Username    string
+	Authorities string
+	Verbose     bool
+	Help        bool
 }
 
 func PullProducts(options *PullProductsOptions) {
 	if options.Verbose {
 		log.Printf("Pulling products from referential API")
 	}
-	client.SendMessage("cnp.referential", "product.pull", "", options.Verbose)
+	headers := amqp.Table{
+		"App-Username":    options.Username,
+		"App-Authorities": options.Authorities,
+	}
+	body := "{}"
+	client.SendMessageWithHeaders("cnp.referential", "product.pull", body, headers, options.Verbose)
 }
 
 func PullProductsFlagSet(options *PullProductsOptions) *flag.FlagSet {
 	fs := flag.NewFlagSet(PullProductsCmd, flag.ExitOnError)
 	fs.BoolVar(&options.Verbose, "v", false, "Verbose")
 	fs.BoolVar(&options.Help, "help", false, "Help")
+	fs.StringVar(&options.Username, "u", "", "Username")
+	fs.StringVar(&options.Authorities, "a", "", "Authorities")
 	return fs
 }
