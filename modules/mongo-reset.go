f78655c584bb328bd6f42c8e6665053d4c8fa407
Luis Cabrera <luis.barragan.gfi@cnppartners.eu>
2019-08-09 10:08:43 +0200

    Fix code smell

--- modules/mongo-reset.go
+++ modules/mongo-reset.go
@@ -20,8 +20,6 @@ type MongoResetOptions struct {
 }
 
 func MongoReset(cmdOptions *MongoResetOptions) {
-	//TODO read confirmation
-
 	mongoUri, definedUri := os.LookupEnv("APP_MONGO_URI")
 	if !definedUri {
 		mongoUri = "mongodb://localhost:27017"
@@ -32,7 +30,9 @@ func MongoReset(cmdOptions *MongoResetOptions) {
 		log.Printf("Cleaning documents")
 	}
 
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
 	if err != nil {
 		log.Fatalf("%s: %s", "Error opening connection", err)
