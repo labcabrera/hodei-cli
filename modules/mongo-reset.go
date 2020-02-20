03c382e03a42d1dbbccc543e343be83979941353
Luis Cabrera <luis.barragan.gfi@cnppartners.eu>
2020-02-20 17:04:01 +0100

    Updated tables

--- modules/mongo-reset.go
+++ modules/mongo-reset.go
@@ -73,11 +73,11 @@ func mongoReset(cmdOptions *mongoExecutionOptions) {
 	}
 
 	remove("cnp-actions", "actions", client)
-	remove("cnp-actions", "scheduledActions", client)
 	remove("cnp-actions", "archivedActions", client)
 	remove("cnp-claims", "claims", client)
 	remove("cnp-coverages", "coverages", client)
 	remove("cnp-customers", "persons", client)
+	remove("cnp-customers", "archivedPersons", client)
 	remove("cnp-documents", "documentCollections", client)
 	remove("cnp-documents", "signatureCallbacks", client)
 	remove("cnp-orders", "orders", client)
@@ -87,6 +87,7 @@ func mongoReset(cmdOptions *mongoExecutionOptions) {
 	remove("ppi-policies", "archivedPolicies", client)
 	remove("pp-policies", "policies", client)
 	remove("pp-policies", "archivedPolicies", client)
+	remove("pp-policies", "policySyncExecutions", client)
 
 	if cmdOptions.verbose {
 		log.Printf("Reset complete")
